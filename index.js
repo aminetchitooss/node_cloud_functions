6b287a9ef3a7203a046c28ff0f48ab73696f07bb
amine.tchitooss <amine.tchitooss@gmail.com>
2020-06-01 19:19:37 +0200

    setup and config

---
+++ index.js
@@ -0,0 +1,16 @@
+const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+const serviceAccount = require("./config.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
+});
+const db = admin.firestore()
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    // db.collection('test').doc("123").set({name:"test"})
+    response.end("Hello from Firebase!");
+});
